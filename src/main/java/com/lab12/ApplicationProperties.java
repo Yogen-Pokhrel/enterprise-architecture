b3ba73e6049f1bc63e0af2d6f179528464d72740
Yogen <yogendrapokhrel777@gmail.com>
2024-07-09 16:02:37 -0500

    Lab 12 part 3c completed

--- src/main/java/com/lab12/ApplicationProperties.java
+++ src/main/java/com/lab12/ApplicationProperties.java
@@ -1,17 +1,26 @@
 package com.lab12;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 
 @ConfigurationProperties(prefix = "demo")
 @Getter
 @Setter
+@Validated
 public class ApplicationProperties {
+    @NotBlank
     private String applicationName;
+
+    @NotBlank
     private String version;
+
+    @NotBlank
     private String serverUrl;
     private String serverName;
     private List<String> countries;
@@ -22,7 +31,13 @@ public class ApplicationProperties {
     class User {
         private String firstName;
         private String LastName;
+
+        @NotBlank
+        @Size(min = 8, max = 15, message = "Username must be between 8 and 15 characters")
         private String userName;
+
+        @NotBlank
+        @Size(min = 8, max = 15, message = "Password must be between 8 and 15 characters")
         private String password;
 
         @Override
