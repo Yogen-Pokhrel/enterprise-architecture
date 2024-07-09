918fe7d1c5cba946fa9c6092c9c9ba09f0d39a4b
Yogen <yogendrapokhrel777@gmail.com>
2024-07-08 22:45:41 -0500

    part 2 completed

---
+++ src/main/java/com/lab11/Customer.java
@@ -0,0 +1,21 @@
+package com.lab11;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Customer {
+	private String name;
+	public Customer() {}
+	public Customer(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer{" +
+				"name='" + name + '\'' +
+				'}';
+	}
+}
---
+++ src/main/java/com/lab11/Lab11Application.java
@@ -0,0 +1,13 @@
+package com.lab11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab11Application.class, args);
+	}
+
+}
