26bb1aae749e48babb2c3fdd2b13376bc6f953db
Yogen <yogendrapokhrel777@gmail.com>
2024-07-09 11:09:13 -0500

    part d missing parts completed

--- src/main/java/bank/dto/request/TransactionDto.java
+++ src/main/java/bank/dto/request/TransactionDto.java
@@ -18,4 +18,6 @@ public class TransactionDto {
     @ValidEnum(enumClass = TransactionAction.class, message = "Please provide a valid action type")
     private String action;
 
+    private long accountNumber;
+
 }
---
+++ src/main/java/bank/kafka/Receiver.java
@@ -0,0 +1,65 @@
+package bank.kafka;
+
+import bank.common.ApiResponse;
+import bank.common.exception.NoResourceFoundException;
+import bank.configuration.Currency;
+import bank.dto.request.CreateAccountDto;
+import bank.dto.request.TransactionAction;
+import bank.dto.request.TransactionDto;
+import bank.dto.request.TransferFundsDto;
+import bank.service.AccountService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class Receiver {
+
+    @Autowired
+    AccountService accountService;
+
+    @KafkaListener(topics = {"createAccount"})
+    public void createAccount(@Payload String data) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            CreateAccountDto createAccountDto = objectMapper.readValue(data, CreateAccountDto.class);
+            accountService.createAccount(createAccountDto.getAccountNumber(), createAccountDto.getCustomerName());
+        } catch (Exception e) {
+            System.out.println("Kafka receiver: Cannot convert : " + data +" to a CreateAccountDto object");
+        }
+    }
+
+    @KafkaListener(topics = {"transfer"})
+    public void transferFunds(@Payload String data) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            TransferFundsDto transferFundsDto = objectMapper.readValue(data, TransferFundsDto.class);
+            accountService.transferFunds(transferFundsDto.getFromAccountId(), transferFundsDto.getToAccountId(),transferFundsDto.getAmount(), transferFundsDto.getDescription());
+        } catch (Exception e) {
+            System.out.println("Kafka receiver: Cannot convert : " + data +" to a TransferFundsDto object");
+        }
+    }
+
+    @KafkaListener(topics = {"transaction"})
+    public void transaction(@Payload String data) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            TransactionDto transactionDto = objectMapper.readValue(data, TransactionDto.class);
+            TransactionAction action = TransactionAction.valueOf(transactionDto.getAction());
+            Currency currency = Currency.valueOf(transactionDto.getCurrency());
+            if(action == TransactionAction.DEPOSIT){
+                accountService.deposit(transactionDto.getAccountNumber(), transactionDto.getAmount(), currency);
+            }else if(action == TransactionAction.WITHDRAW){
+                accountService.withdraw(transactionDto.getAccountNumber(), transactionDto.getAmount(), currency);
+            }else{
+                throw new NoResourceFoundException("No resource found for action: " + action);
+            }
+        } catch (Exception e) {
+            System.out.println("Kafka receiver: Cannot convert : " + data +" to a TransferFundsDto object");
+        }
+    }
+}
