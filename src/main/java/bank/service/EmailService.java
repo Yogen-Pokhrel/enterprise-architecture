d3ef39eef0b5bd654dbb23344ca9b7c9a21a8586
Yogen <yogendrapokhrel777@gmail.com>
2024-07-09 23:17:02 -0500

    Email service called asynchronously

--- src/main/java/bank/service/EmailService.java
+++ src/main/java/bank/service/EmailService.java
@@ -5,9 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 @Service
+@EnableAsync
 public class EmailService {
 
     @Autowired
@@ -15,6 +18,7 @@ public class EmailService {
 
     Logger logger = LoggerFactory.getLogger(EmailService.class);
 
+    @Async
     @EventListener
     public void onAccountChangeEvent(AccountEventDto accountDto) {
         logger.info("Account changed event received: ");
